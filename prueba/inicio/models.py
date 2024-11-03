4da0943c5ff91ff7ee74f5a2e91bef102b3b2b1c
ericksiu <erickcruzrobles>
2024-11-03 12:58:51 -0600

    Modelos Erick

--- prueba/inicio/models.py
+++ prueba/inicio/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+#Modelos hechos por Erick
 class Grupos (models.Model):
     id_grupo = models.IntegerField(max_length=10000, verbose_name="ID", primary_key=True)
     nombre = models.CharField(max_length=100, verbose_name="Nombre del grupo")
@@ -28,80 +29,4 @@ class Reseñas(models.Model):
 
     def __str__(self):
         return self.comentario
-    
-class Libros(models.Model):
-    id_libro = models.IntegerField(max_length=10000, verbose_name="ID: ", primary_key=True)
-    ISBN = models.CharField(max_length=100, verbose_name="ISBN: ")
-    nombre = models.CharField(max_length=100, verbose_name="Nombre: ")
-    autor = models.CharField(max_length=100, verbose_name="Autor: ")
-    año = models.DateField(max_length=100, verbose_name="Año: ")
-
-    class Meta:
-        verbose_name = "Libro"
-        verbose_name_plural = "Libros"
-        ordering = ["-created"]
-
-    def __str__(self):
-        return self.nombre
-    
-class Usuarios(models.Model):
-    id_usuario = models.IntegerField(max_length=10000, verbose_name="ID: ", primary_key=True)
-    id_grupo = models.ForeignKey(Grupos, on_delete=models.CASCADE, verbose_name="Grupo: ")
-    nombre = models.CharField(max_length=100, verbose_name="Nombre: "),
-    email = models.EmailField(max_length=100, verbose_name="Correo electrónico: ")
-    contraseña = models.CharField(max_length=100, verbose_name="Contraseña: ")
-
-    class Meta:
-        verbose_name = "Usuario"
-        verbose_name_plural = "Usuarios"
-        ordering = ["-created"]
-
-    def __str__(self):
-        return self.nombre
-
-class Categorias(models.Model):
-    id_categoria = models.IntegerField(max_length=10000, verbose_name="ID: ", primary_key=True)
-    nombre = models.CharField(max_length=100, verbose_name="Nombre: ")
-
-    class Meta:
-        verbose_name = "Categoría"
-        verbose_name_plural = "Categorías"
-        ordering = ["-created"]
-
-    def __str__(self):
-        return self.nombre
-
-
-class Libros_Categorias(models.Model):
-    id_categoria = models.IntegerField(max_length=10000, verbose_name="ID: ", primary_key=True)
-    id_libro= models.ForeignKey(Libros, on_delete=models.CASCADE, verbose_name="Libros: ")
-    id_libro_categoria=models.ForeignKey(Categorias, on_delete=models.CASCADE, verbose_name="ID:")
-
-    class Meta:
-        verbose_name = "Libros Categorías"
-        verbose_name_plural = "Libros Categorías"
-        ordering = ["-created"]
-
-    def __str__(self):
-        return self.id_categoria 
-
-class Descargas(models.Model):
-    id_descarga = models.IntegerField(max_length=10000, verbose_name="ID: ", primary_key=True)
-    id_libro = models.ForeignKey(Libros, on_delete=models.CASCADE, verbose_name="ID_Libro: ")
-    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE, verbose_name="ID_Usuario")
-    fecha = models.DateField(auto_now_add=True, verbose_name="Fecha de descarga: ")
-
-    class Meta:
-        verbose_name = "Descarga"
-        verbose_name_plural = "Descargas"
-        ordering = ["-created"]
-
-    def __str__(self):
-        return self.id_descarga
-class Ver_Libros(models.Model):
-    id_ver_libro = models.IntegerField(max_length=10000, verbose_name="ID: ", primary_key=True)
-    id_libro = models.ForeignKey(Libros, on_delete=models.CASCADE, verbose_name="ID_Libro: ")
-    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE, verbose_name="ID_Usuario")
-    fecha = models.DateField(auto_now_add=True)
-
-
+  
