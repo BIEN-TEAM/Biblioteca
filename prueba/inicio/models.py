723c3402bfa95de3426187dedcb5770148b62a98
Zellenson <utm210081ti@ut-morelia.edu.mx>
2024-11-04 20:14:12 -0600

    Modelos-Categorias

--- prueba/inicio/models.py
+++ prueba/inicio/models.py
@@ -65,7 +65,6 @@ class Reseñas(models.Model):
     def __str__(self):
         return self.comentario
 
-
 class Categorias(models.Model):
     id_categoria = models.AutoField(verbose_name="ID: ", primary_key=True)  # Usar AutoField
     nombre = models.CharField(max_length=100, verbose_name="Nombre: ")
@@ -76,7 +75,7 @@ class Categorias(models.Model):
         verbose_name_plural = "Categorías"
         ordering = ["-created"]
 
-    def __str__(self):
+    def _str_(self):
         return self.nombre
 
 
@@ -87,43 +86,13 @@ class Libros_Categorias(models.Model):
     created = models.DateTimeField(auto_now_add=True)  # Agregar campo created
 
     class Meta:
-        verbose_name = "Libros Categorías"
+        verbose_name = "Libros Categoría"
         verbose_name_plural = "Libros Categorías"
         ordering = ["-created"]
 
-    def __str__(self):
-        return str(self.id_categoria)  # Convertir a string para evitar errores
+    def _str_(self):
+        return str(self.id_categoria)
 
 
-class Descargas(models.Model):
-    id_descarga = models.AutoField(verbose_name="ID: ", primary_key=True)  # Usar AutoField
-    id_libro = models.ForeignKey(Libros, on_delete=models.CASCADE, verbose_name="ID_Libro: ")
-    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE, verbose_name="ID_Usuario")
-    fecha = models.DateField(auto_now_add=True, verbose_name="Fecha de descarga: ")
-    created = models.DateTimeField(auto_now_add=True)  # Agregar campo created
 
-    class Meta:
-        verbose_name = "Descarga"
-        verbose_name_plural = "Descargas"
-        ordering = ["-created"]
-
-    def __str__(self):
-        return str(self.id_descarga)
-
-
-class Ver_Libros(models.Model):
-    id_ver_libro = models.AutoField(verbose_name="ID: ", primary_key=True)  # Usar AutoField
-    id_libro = models.ForeignKey(Libros, on_delete=models.CASCADE, verbose_name="ID_Libro: ")
-    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE, verbose_name="ID_Usuario")
-    fecha = models.DateField(auto_now_add=True)
-    created = models.DateTimeField(auto_now_add=True)  # Agregar campo created
-
-    class Meta:
-        verbose_name = "Ver Libro"
-        verbose_name_plural = "Ver Libros"
-        ordering = ["-created"]
-
-    def __str__(self):
-        return str(self.id_ver_libro)
-  
 
